8e77b0b84ed6ac7f439043039429321430526a65
Ivan Vinogradov <vinogradov_@ukr.net>
2022-03-31 14:53:53 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+/*
+let number = 5; //Переменная изменяема
+const LeftBorderWidth = 1; //Переменная постоянная
+number = 10;
+console.log(number);
+*/
+
+
+// Обьекты и обращение кним
+/*const person = {
+    name: "Ivan",
+    age: 35,
+};
+
+console.log(person.name);
+*/
+// Массивы и обращения к ним
+//let arr =["plum", "orange", "grape", "apple"];
+
+//console.log(arr[2]);
+
+//typeof() - приверка на тип данных
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your lastname?', '');
+answers[2] = prompt('whoy old are you?', '');
+
+document.write(answers);
